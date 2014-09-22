293aec8c36e704496ea3e63b12747051da6f45f1
vchrisb <vchrisb@users.noreply.github.com>
2014-09-22 14:15:36 +0200

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -5,6 +5,6 @@ python manage.py migrate --noinput
 #echo "import sample data"
 #python manage.py loaddata vLab.json
 
-echo "create defaul admin user"
+echo "create default admin user"
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@vlab.local', 'Passw0rd')" | python manage.py shell
 gunicorn emcforum.wsgi --workers 2
