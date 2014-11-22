8c3fa6d00d55bcfe4b2cacb59a65adf7453dba4e
vchrisb <vchrisb@users.noreply.github.com>
2014-11-22 20:21:06 +0100

    migrated to whitenoise for static file serving

--- emcforum/wsgi.py
+++ emcforum/wsgi.py
@@ -6,9 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-from dj_static import Cling
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emcforum.settings")
 
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
-application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
